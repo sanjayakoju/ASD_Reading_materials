65dd59cfa7ed938c3804758b891319fc661bf9ea
sanjaya.koju <sanjaya.koju@blankfactor.com>
2023-08-03 21:44:17 -0700

    Lesson 6

---
+++ lesson-6/statepattern/src/main/java/frequent/flyer/with/state/AccountState.java
@@ -0,0 +1,13 @@
+package frequent.flyer.with.state;
+
+public abstract class AccountState {
+	protected final FFAccount account;
+
+	public AccountState(FFAccount account) {
+		this.account = account;
+	}
+
+	public abstract void addFlight(int newMiles);
+
+	public abstract String getAccountType();
+}
